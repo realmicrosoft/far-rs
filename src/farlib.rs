f17016906e0cda22f27930e4d7c1e534c0a8a578
husky <husky@gaycatboy.sex>
2022-04-10 01:14:24 -0700

    extraction added

--- src/farlib.rs
+++ src/farlib.rs
@@ -1,11 +1,59 @@
 use std::io::{BufRead, BufReader, Read};
 
+pub struct FarFileInfo {
+    pub name: String,
+    pub size: u32,
+    offset: u32,
+}
+
+pub struct FarFile {
+    pub name: String,
+    pub size: u32,
+    pub data: Vec<u8>,
+}
+
 pub struct FarArchive {
-    pub version: u8
+    pub version: u8,
+    pub file_count: u32,
+    pub file_list: Vec<FarFileInfo>,
+    pub file_data: Vec<FarFile>,
+}
+
+impl FarFile {
+    pub fn new_from_archive(name : String, size : u32, offset : u32, original_file : &Vec<u8>) -> FarFile {
+        let mut reader = BufReader::new(&original_file[offset as usize..(offset + size) as usize]);
+        let mut data = Vec::new();
+        reader.read_to_end(&mut data);
+        FarFile {
+            name,
+            size,
+            data,
+        }
+    }
+}
+
+impl FarArchive {
+    pub fn load_file_data(self, original_file : &Vec<u8>) -> FarArchive {
+        let mut new_file_data = Vec::new();
+        for i in 0..self.file_list.len() {
+            new_file_data.push(FarFile::new_from_archive(
+                self.file_list[i].name.clone(),
+                self.file_list[i].size,
+                self.file_list[i].offset,
+                original_file,
+            ));
+        }
+        FarArchive {
+            version: self.version,
+            file_count: self.file_count,
+            file_list: self.file_list,
+            file_data: new_file_data,
+        }
+    }
 }
 
 // test for FAR!byAZ
-pub fn test(file : Vec<u8>) -> Result<FarArchive, String> {
+pub fn test(file : &Vec<u8>) -> Result<FarArchive, String> {
     let mut reader = BufReader::new(&file[..]);
     let mut magic = [0u8; 8];
     reader.read_exact(&mut magic).unwrap();
@@ -14,7 +62,51 @@ pub fn test(file : Vec<u8>) -> Result<FarArchive, String> {
     }
     let mut version = [0; 1];
     reader.read_exact(&mut version).unwrap();
+    let version = version[0];
+    // get list of files
+    let files = list_files(file).expect("Failed to list files");
     Ok(FarArchive {
-        version: version[0]
+        version,
+        file_count: files.len() as u32,
+        file_list: files,
+        file_data: vec![],
     })
+}
+
+pub fn list_files(file : &Vec<u8>) -> Result<Vec<FarFileInfo>, String> {
+    // manifest offset is at 12 bytes (u32)
+    let mut reader = BufReader::new(&file[12..]);
+    let mut offset = [0u8; 4];
+    reader.read_exact(&mut offset).unwrap();
+    let offset = u32::from_le_bytes(offset);
+    // move to manifest
+    reader = BufReader::new(&file[offset as usize..]);
+    // read u32 for number of files
+    let mut num_files = [0u8; 4];
+    reader.read_exact(&mut num_files).unwrap();
+    let num_files = u32::from_le_bytes(num_files);
+    // for each file, read u32 for size, u32 for size again (stored twice for some reason), u32 for offset, u32 for name length, name
+    let mut files = Vec::new();
+    for i in 0..num_files {
+        let mut size = [0u8; 4];
+        reader.read_exact(&mut size).expect(format!("Failed to read size for file {}", i).as_str());
+        let size = u32::from_le_bytes(size);
+        let mut size2 = [0u8; 4];
+        reader.read_exact(&mut size2).expect(format!("Failed to read size for file {}", i).as_str());
+        let size2 = u32::from_le_bytes(size2);
+        let mut offset = [0u8; 4];
+        reader.read_exact(&mut offset).expect(format!("Failed to read offset for file {}", i).as_str());
+        let offset = u32::from_le_bytes(offset);
+        let mut name_len = [0u8; 4];
+        reader.read_exact(&mut name_len).expect(format!("Failed to read name length for file {}", i).as_str());
+        let name_len = u32::from_le_bytes(name_len);
+        let mut name = vec![0u8; name_len as usize];
+        reader.read_exact(&mut name).expect(format!("Failed to read name for file {}", i).as_str());
+        files.push(FarFileInfo {
+            name: String::from_utf8(name).unwrap(),
+            size,
+            offset,
+        });
+    }
+    Ok(files)
 }
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod farlib;
 
 use std::{env, fs};
 use std::fs::File;
+use std::io::Write;
 
 fn print_help(args : Vec<String>) {
     println!("Usage: {} <command> <archive name> [FILES...]", args[0]);
@@ -9,6 +10,7 @@ fn print_help(args : Vec<String>) {
     println!("  help - show this help message");
     println!("  test - test if file is a valid archive");
     println!("  list - list files in archive");
+    println!("  extract - extract files from archive to current directory (will make a new directory)");
     return;
 }
 
@@ -25,8 +27,8 @@ fn main() {
             print_help(args);
         },
         "test" => {
-            let mut file = fs::read(archive_name).expect("Failed to read file ");
-            let test = farlib::test(file);
+            let mut file = fs::read(archive_name).expect("Failed to read file");
+            let test = farlib::test(&file);
             match test {
                 Ok(archive_obj) => {
                     println!("{} is a valid archive", archive_name);
@@ -37,6 +39,43 @@ fn main() {
                 }
             }
         },
+        "list" => {
+            let file = fs::read(archive_name).expect("Failed to read file");
+            let test = farlib::test(&file);
+            match test {
+                Ok(archive_obj) => {
+                    let files = archive_obj.file_list;
+                    for file in files {
+                        println!("{} ({} bytes)", file.name, file.size);
+                    }
+                },
+                Err(e) => {
+                    println!("{} is not a valid archive: {}", archive_name, e);
+                }
+            }
+        },
+        "extract" => {
+            let file = fs::read(archive_name).expect("Failed to read file");
+            let test = farlib::test(&file);
+            match test {
+                Ok(archive_obj) => {
+                    let archive_with_data = archive_obj.load_file_data(&file);
+                    // make a new directory
+                    let dir_path = format!("{}/{}", env::current_dir().unwrap().to_str().unwrap(), archive_name.split(".").next().unwrap());
+                    fs::create_dir_all(dir_path.clone()).expect("Failed to create directory");
+                    let files = archive_with_data.file_data;
+                    for file in files {
+                        let mut file_path = format!("{}/{}", dir_path.clone(), file.name);
+                        let mut file_data = file.data;
+                        let mut file_handle = File::create(file_path).expect("Failed to create file");
+                        file_handle.write_all(&file_data).expect("Failed to write file");
+                    }
+                },
+                Err(e) => {
+                    println!("{} is not a valid archive: {}", archive_name, e);
+                }
+            }
+        }
         _ => {
             println!("Unknown command: {}", command);
         }
